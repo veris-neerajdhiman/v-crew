b9b12bdf61869cd758808380b4bb05debcbf28a2
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-04-28 17:41:31 +0530

    refactor : unwanted code removed :fire:

--- apps/organization/serializers.py
+++ apps/organization/serializers.py
@@ -13,11 +13,11 @@ from __future__ import unicode_literals
 
 # 3rd party
 from rest_framework import serializers
-from datetime import datetime
 
 # Django
 
 # local
+from apps.members.models import Member
 
 # own app
 from apps.organization import models
@@ -33,33 +33,3 @@ class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Organization
         exclude = ('id', 'token', 'created_at', 'modified_at',)
-
-    # def create(self, validated_data):
-    #     """
-    #
-    #     :param validated_data: Validated data
-    #     :return: Organization instance
-    #     """
-    #     created_at = modified_at = datetime.now()
-    #
-    #     validated_data.update({
-    #         'created_at': created_at,
-    #         'modified_at': modified_at
-    #     })
-    #
-    #     return super(OrganizationSerializer, self).create(validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     """
-    #
-    #     :param instance: Organization instance to be updated
-    #     :param validated_data: Validated data
-    #     :return: Organization instance
-    #     """
-    #     modified_at = datetime.now()
-    #
-    #     validated_data.update({
-    #         'modified_at': modified_at
-    #     })
-    #
-    #     return super(OrganizationSerializer, self).update(instance, validated_data)
