74e370b2183d8ebea65a404457746391da03bc83
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-07-26 19:00:27 +0530

    Update : Organization owner permission :construction_worker:

--- apps/permissions.py
+++ apps/permissions.py
@@ -12,7 +12,6 @@
 from rest_framework import permissions, exceptions, status
 
 # local
-from apps import policy
 from apps.organization.models import Organization
 
 
@@ -24,58 +23,8 @@ class CustomForbidden(exceptions.APIException):
     default_detail = "You are not authorized to perform this action."
 
 
-class ValidateOrgnizationPermission(permissions.BasePermission):
-    """
-
-    """
-
-    def has_permission(self, request, view):
-        """
-
-        """
-        action = None
-        if request.method == 'POST':
-            action = 'create'
-        elif request.method in ('GET', 'PATCH', 'DELETE'):
-            action = 'read'
-        user_uuid = request.parser_context.get('kwargs').get('owner')
-        org_uuid = request.parser_context.get('kwargs').get('token')
-
-        if not policy.check_user_policy_for_organization(user_uuid, org_uuid, action):
-            raise CustomForbidden
-        return True
-
-
-class ValidateMemberPermission(permissions.BasePermission):
-    """
-
-    """
-
-    def has_permission(self, request, view):
-        """
-
-        """
-        action = None
-        if request.method == 'POST':
-            action = 'create'
-        elif request.method in ('GET', 'PATCH', 'DELETE'):
-            action = 'read'
-        user_uuid = request.parser_context.get('kwargs').get('owner')
-
-        # exceptional case when member is trying to access his memberships but as user (logged in  from app)
-        if user_uuid is None:
-            user_uuid = request.parser_context.get('kwargs').get('member_user_uuid')
-        org_uuid = request.parser_context.get('kwargs').get('organization')
-        member_uuid = request.parser_context.get('kwargs').get('uuid')
-
-        if not policy.check_user_policy_for_member(user_uuid, org_uuid, member_uuid, action):
-            raise CustomForbidden
-        return True
-
-
 class IsUserOrganizationOwner(permissions.BasePermission):
-    """Here we will verify user UUID used in url w.r.t Organization in url is actually UUID of that Organization
-    Owner UUId
+    """Here we will verify user to0ken used to access API is actually the owner of Organization.
 
     """
 
@@ -84,7 +33,7 @@ class IsUserOrganizationOwner(permissions.BasePermission):
 
         """
         filters = {
-            'owner': request.parser_context['kwargs'].get('owner'),
+            'user': request.user,
             'token': request.parser_context['kwargs'].get('organization')
         }
         if not Organization.objects.filter(**filters):
